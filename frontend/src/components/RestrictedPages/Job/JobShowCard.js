1ebb3b3733ef7eebeeeaa312f2f1c2c8d8151367
KirtikumarKavande <kirtikumar0005@gmail.com>
2023-12-11 19:11:56 +0530

    feat:showed available jobs on job page

--- frontend/src/components/RestrictedPages/Job/JobShowCard.js
+++ frontend/src/components/RestrictedPages/Job/JobShowCard.js
@@ -4,6 +4,8 @@ import { MdCurrencyRupee } from "react-icons/md";
 import { IoLocationOutline } from "react-icons/io5";
 import { TbFileDescription } from "react-icons/tb";
 import { GoDotFill } from "react-icons/go";
+import ReactHtmlParser from "html-react-parser";
+
 const JobShowCard = (props) => {
   const {
     companyName,
@@ -14,6 +16,8 @@ const JobShowCard = (props) => {
     skills,
     jobDescription,
   } = props;
+
+  console.log("lets see", ReactHtmlParser(jobDescription));
   return (
     <div className="block p-6 w-full bg-white border border-gray-200 mb-3 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
       <h3 className="font-semibold text-lg">{jobTitle}</h3>
@@ -34,7 +38,7 @@ const JobShowCard = (props) => {
       <div className="flex items-center text-gray-600 mt-2">
         <TbFileDescription size={25} />{" "}
         <div className="w-full whitespace-nowrap overflow-hidden text-ellipsis ">
-          Click Here to see Detailed Job Description...
+          {ReactHtmlParser(jobDescription)[4]}
         </div>
       </div>
       <div>
